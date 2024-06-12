49ea258bb0c865026a4b94b5a497c51b00941419
Manu VG <manu.gvm@gmail.com>
2024-06-12 22:56:30 +0200

    Got pylsp with ruff to work.

--- initfiles/init-python.el
+++ initfiles/init-python.el
@@ -1,63 +1,39 @@
 ;;;========================================
-;;; eglot
-;;;========================================
-
-(use-package eglot
-  :defer t
-  :bind (:map eglot-mode-map
-              ("C-c h" . eldoc)
-              ("C-c C-e" . eglot-rename)
-              ("C-c C-f" . eglot-format-buffer))
-  :hook ((python-ts-mode . eglot-ensure)))
-
-;;;========================================
 ;;; Python
 ;;;========================================
-;; (use-package pyvenv
-;;   :ensure t
-;;   :config
-;;   (pyvenv-activate "/home/manugv/pythonenv/base/"))
+ 
+(use-package python
+  :config
+  (setq python-indent-offset 4)
+  ;; Python interactive shells
+   (setq python-shell-interpreter "ipython"
+    	 python-shell-interpreter-args "--simple-prompt")
+   (setq-default eglot-workspace-configuration
+		 '((:pylsp . (:configurationSources ["flake8"]
+			      :plugins (:flake8 (:enabled :json-false)
+					:black (:enabled :json-false :line_length 100 :cache_config t)
+					:mccabe (:enabled t  :threshold 15)
+					:pyflakes (:enabled :json-false)
+					:yapf (:enabled :json-false)
+					:pydocstyle (:enabled :json-false :convention "numpy")
+					:rope (:enabled :json-false)
+					:ruff (:enabled t :lineLength 100)
+					:pycodestyle (:enabled :json-false)
+					:autopep8 (:enabled :json-false))))))
+   :hook
+   (python-mode . hs-minor-mode)
+   )
 
 ;; numpy docstring for python
 (use-package numpydoc
   :ensure t
   :after python
-  :config  (setq numpydoc-insertion-style 'yas)
-  :bind (:map python-ts-mode-map
+  ;; :config  (setq numpydoc-insertion-style 'nil)
+  :bind (:map python-mode-map
 	      ("C-c C-n" . numpydoc-generate)))
 
-
-(use-package python
-  :config
-  (setq python-indent-offset 4)
-   ;;Python interactive shells
-   (setq python-shell-interpreter "ipython"
-    	 python-shell-interpreter-args "--simple-prompt")
-   :hook
-   ((python-mode . pyvenv-mode)
-    (python-mode . hs-minor-mode)
-    (python-mode . eglot-ensure)
-    (python-mode . (lambda () (set-fill-column 120))))
-   :config
-   (setq-default eglot-workspace-configuration
-		 '((:pylsp . (:configurationSources ["flake8"]
-	     		      :plugins (
-				:pycodestyle (:enabled :json-false)
-				:flake8 (:enabled :json-false :maxLineLength 120)
-				:black (:enabled :json-false :line_length 100 :cache_config t)
-				:mccabe (:enabled t  :threshold 15)
-				:pyflakes (:enabled :json-false)
-				:ruff (:enabled t);; :lineLength 100)
-				:yapf (:enabled t)
-				:pydocstyle (:enabled :json-false :convention "numpy")
-				:rope (:enabled :json-false)
-				:autopep8 (:enabled :json-false))))))
-   )
 ;; Open  in tree sitter mode
 (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
-
-;; (with-eval-after-load 'eglot
-;;   (add-to-list 'eglot-server-programs
-;;                '(python-ts-mode . ("ruff" "server" "--preview"))))
+(add-hook 'python-ts-mode-hook 'eglot-ensure)
 
 (provide 'init-python)
