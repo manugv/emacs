c54fd3745d0db5a5fd1ab5ce5d183df496e338c2
Manu VG <manu.gvm@gmail.com>
2024-08-01 21:40:41 +0200

    Change Vertico to fido and icomplete mode.

--- initfiles/custom.el
+++ initfiles/custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(icomplete-separator " * ")
  '(package-selected-packages
-   '(bind-key eglot eldoc faceup flymake idlwave jsonrpc org project soap-client tramp use-package verilog-mode xref eldoc-box envrc yaml-mode which-key vterm vertico rust-mode orderless numpydoc nerd-icons-dired nerd-icons-corfu nerd-icons-completion marginalia magit ledger-mode julia-repl julia-mode embark-consult eglot-jl corfu cargo cape auctex async))
+   '(org-superstar bind-key eglot eldoc faceup flymake idlwave jsonrpc org project soap-client tramp use-package verilog-mode xref envrc yaml-mode which-key vterm rust-mode numpydoc nerd-icons-dired nerd-icons-corfu nerd-icons-completion marginalia magit ledger-mode julia-repl julia-mode eglot-jl corfu cargo cape auctex async))
  '(tool-bar-mode nil))
 ;; (custom-set-faces
 ;;  ;; custom-set-faces was added by Custom.
--- initfiles/init-completion.el
+++ initfiles/init-completion.el
@@ -5,14 +5,25 @@
 ;; need to modify its behaviour.
 ;;
 ;; Further reading: https://protesilaos.com/emacs/dotemacs#h:cff33514-d3ac-4c16-a889-ea39d7346dc5
-(use-package vertico
-  :ensure t
-  :config
-  (setq vertico-cycle t
-	vertico-resize nil
-	vertico-count 5)
-  (vertico-mode 1))
+;; (use-package vertico
+;;   :ensure t
+;;   :config
+;;   (setq vertico-cycle t
+;; 	vertico-resize nil
+;; 	vertico-count 5)
+;;   (vertico-mode 1))
 
+;; Fido (icomplete now includes fuzzy matching for M-x and other completions)
+(use-package icomplete
+  :demand t
+  :config
+  (add-hook 'icomplete-minibuffer-setup-hook
+	    (lambda () (setq-local max-mini-window-height 10)))
+  (setq-local completion-styles '(substring initials flex))
+  (fido-vertical-mode 1)
+  :bind (:map icomplete-fido-mode-map
+	      ("RET" . icomplete-fido-ret)
+	      ("TAB" . icomplete-force-complete)))
 
 ;; The `marginalia' package provides helpful annotations next to
 ;; completion candidates in the minibuffer.  The information on
@@ -37,16 +48,13 @@
 ;; algorithms), but let us keep things simple.
 ;;
 ;; Further reading: https://protesilaos.com/emacs/dotemacs#h:7cc77fd0-8f98-4fc0-80be-48a758fcb6e2
-(use-package orderless
-  :ensure t
-  :config
-  (setq completion-styles '(orderless basic)))
+;; (use-package orderless
+;;   :ensure t
+;;   :config
+;;   (setq completion-styles '(orderless basic)))
 
 
-;; The built-in `savehist-mode' saves minibuffer histories.  Vertico
-;; can then use that information to put recently selected options at
-;; the top.
-;;
+;; The built-in `savehist-mode' saves minibuffer histories.  Vert
 ;; Further reading: https://protesilaos.com/emacs/dotemacs#h:25765797-27a5-431e-8aa4-cc890a6a913a
 (savehist-mode 1)
 
@@ -56,7 +64,20 @@
 ;;
 ;; I do not use this facility, because the files I care about are
 ;; either in projects or are bookmarked.
-(recentf-mode 1)
+(use-package recentf
+  :config
+  (setq recentf-auto-cleanup 'never) ;; prevent issues with Tramp
+  (setq recentf-max-saved-items 100)
+  (setq recentf-max-menu-items 15)
+  (recentf-mode t)
+  (defun my/recentf-ido-find-file ()
+    "Find a recent file using ido."
+    (interactive)
+    (let ((file (completing-read "Choose recent file: " recentf-list nil t)))
+      (when file
+        (find-file file))))
+  
+  :bind ("C-x f" . my/recentf-ido-find-file))
 
 ;;; Corfu (in-buffer completion popup)
 (use-package corfu
