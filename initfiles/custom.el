214e9f934426569b002da79d9b7e23d228130b0f
Manu Goudar <manu.gvm+jasmine@gmail.com>
2024-10-07 22:52:32 +0200

    Added Modus themes
    Signed-off-by: Manu Goudar <manu.gvm+jasmine@gmail.com>

--- initfiles/custom.el
+++ initfiles/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(icomplete-separator " * ")
  '(package-selected-packages
-   '(org-superstar bind-key eglot eldoc faceup flymake idlwave jsonrpc org project soap-client tramp use-package verilog-mode xref envrc yaml-mode which-key vterm rust-mode numpydoc nerd-icons-dired nerd-icons-corfu nerd-icons-completion marginalia magit ledger-mode julia-repl julia-mode eglot-jl corfu cargo auctex async))
+   '(modus-themes org-superstar bind-key eglot eldoc faceup flymake idlwave jsonrpc org project soap-client tramp use-package verilog-mode xref envrc yaml-mode which-key vterm rust-mode numpydoc nerd-icons-dired nerd-icons-corfu nerd-icons-completion marginalia magit ledger-mode julia-repl julia-mode eglot-jl corfu cargo auctex async))
  '(tool-bar-mode nil))
 ;; (custom-set-faces
 ;;  ;; custom-set-faces was added by Custom.
@@ -13,10 +13,3 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight regular :height 125 :width normal)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eldoc-box-body ((t nil)))
- '(eldoc-box-markdown-separator ((t (:strike-through t :height 0.4 :family "JetBrains Mono")))))
--- initfiles/init-look.el
+++ initfiles/init-look.el
@@ -1,38 +1,45 @@
 ;; Load a theme
 ;;; For the built-in themes which cannot use `require'.
-(require-theme 'modus-themes)
+(use-package modus-themes
+  :ensure t
+  :config
+  ;; Load the theme of your choice.
+  (load-theme 'modus-vivendi-tinted :no-confirm)
+  (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
+  
+;; (require-theme 'modus-themes)
 ;; Add all your customizations prior to loading the themes.
-(setq modus-themes-bold-constructs t
-      modus-themes-completions '((matches . (extrabold intense background))
-                                 (selection . (semibold accented intense))
-                                 (popup . (accented)))
-      modus-themes-diffs 'desaturated
-      modus-themes-headings '((1 . (1.2))
-                              (2 . (rainbow 1.1))
-                              (3 . (1))
-                              (t . (monochrome)))
-      modus-themes-hl-line '(nil)
-      modus-themes-links '(nil)
-      ;; can use mixed fonts
-      modus-themes-mixed-fonts t
-      modus-themes-mode-line '(borderless accented)
-      modus-themes-prompts '(background)
-      modus-themes-region '(accented bg-only)
-      modus-themes-syntax '(faint)
-      modus-themes-tabs-accented nil
-      )
-(setq modus-themes-org-agenda
-      '((header-date . (grayscale workaholic bold-today))
-        (header-block . (1.4 semibold))
-        (scheduled . uniform)
-        (event . (italic))
-        (habit . traffic-light)))
+;; (setq modus-themes-bold-constructs t
+;;       modus-themes-completions '((matches . (extrabold intense background))
+;;                                  (selection . (semibold accented intense))
+;;                                  (popup . (accented)))
+;;       modus-themes-diffs 'desaturated
+;;       modus-themes-headings '((1 . (1.2))
+;;                               (2 . (rainbow 1.1))
+;;                               (3 . (1))
+;;                               (t . (monochrome)))
+;;       modus-themes-hl-line '(nil)
+;;       modus-themes-links '(nil)
+;;       ;; can use mixed fonts
+;;       modus-themes-mixed-fonts t
+;;       modus-themes-mode-line '(borderless accented)
+;;       modus-themes-prompts '(background)
+;;       modus-themes-region '(accented bg-only)
+;;       modus-themes-syntax '(faint)
+;;       modus-themes-tabs-accented nil
+;;       )
+;; (setq modus-themes-org-agenda
+;;       '((header-date . (grayscale workaholic bold-today))
+;;         (header-block . (1.4 semibold))
+;;         (scheduled . uniform)
+;;         (event . (italic))
+;;         (habit . traffic-light)))
 
 ;; Load the theme of your choice.
-(load-theme 'modus-vivendi)
+;; (load-theme 'modus-vivendi)
 ;; Optionally define a key to switch between Modus themes.  Also check
 ;; the user option `modus-themes-to-toggle'.
-(define-key global-map (kbd "<f5>") #'modus-themes-toggle)
+;; (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
 
 
 (set-face-attribute 'default nil
