c03eef08e82bc879d752f8cf2b4547b5dfe58258
Manu Goudar <manu.gvm+jasmine@gmail.com>
2024-10-30 20:31:10 +0100

    minimal theme config

--- initfiles/init-look.el
+++ initfiles/init-look.el
@@ -2,62 +2,28 @@
 ;;; For the built-in themes which cannot use `require'.
 (use-package modus-themes
   :ensure t
+  :demand t
+  :bind (("<f5>" . modus-themes-toggle)
+	 ("C-<f5>" . modus-themes-select))
   :config
   (setq modus-themes-to-toggle '(modus-vivendi-tinted modus-operandi-tinted)
 	modus-themes-mixed-fonts t)
-  
   ;; Load the theme of your choice.
-  (load-theme 'modus-vivendi-tinted :no-confirm)
-  (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
-  
-;; (require-theme 'modus-themes)
-;; Add all your customizations prior to loading the themes.
-;; (setq modus-themes-bold-constructs t
-;;       modus-themes-completions '((matches . (extrabold intense background))
-;;                                  (selection . (semibold accented intense))
-;;                                  (popup . (accented)))
-;;       modus-themes-diffs 'desaturated
-;;       modus-themes-headings '((1 . (1.2))
-;;                               (2 . (rainbow 1.1))
-;;                               (3 . (1))
-;;                               (t . (monochrome)))
-;;       modus-themes-hl-line '(nil)
-;;       modus-themes-links '(nil)
-;;       ;; can use mixed fonts
-;;       modus-themes-mixed-fonts t
-;;       modus-themes-mode-line '(borderless accented)
-;;       modus-themes-prompts '(background)
-;;       modus-themes-region '(accented bg-only)
-;;       modus-themes-syntax '(faint)
-;;       modus-themes-tabs-accented nil
-;;       )
-;; (setq modus-themes-org-agenda
-;;       '((header-date . (grayscale workaholic bold-today))
-;;         (header-block . (1.4 semibold))
-;;         (scheduled . uniform)
-;;         (event . (italic))
-;;         (habit . traffic-light)))
-
-;; Load the theme of your choice.
-;; (load-theme 'modus-vivendi)
-;; Optionally define a key to switch between Modus themes.  Also check
-;; the user option `modus-themes-to-toggle'.
-;; (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
+  (load-theme 'modus-vivendi-tinted :no-confirm))
 
 
+;; fixed-pitch will be same as default.
 (set-face-attribute 'default nil
-		    :family "Fira Code Retina"
+		    :family "Fira Code"
 		    :height 130
 		    :weight 'regular
 		    )
 (set-face-attribute 'variable-pitch nil
- 		    :family "JetBrains Mono"
-		    :height 125
-		    :weight 'regular)
-(set-face-attribute 'fixed-pitch nil
-		    :family "Fira Code"
-		    :height 125
-		    :weight 'regular)
+ 		    :family "JetBrainsMono"
+		    :height 130
+		    :weight 'Regular)
+
+;; make the text mode to use variable pitch
 (add-hook 'text-mode-hook #'variable-pitch-mode)
 
 (provide 'init-look)
