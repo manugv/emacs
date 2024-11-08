70574e5f3da7f4375926350148a334b2d51134fc
Manu VG <manu.gvm@gmail.com>
2024-11-08 21:25:20 +0100

    Remove global envrc-mode.

--- initfiles/init-envrc.el
+++ initfiles/init-envrc.el
@@ -3,8 +3,8 @@
 (use-package envrc
   :if (executable-find "direnv")        ; Only load envrc if direnv is installed
   :ensure t
-  :hook (after-init . envrc-global-mode)
-  ;; (prog-mode . envrc-mode)
+  :hook (;;(after-init . envrc-global-mode)
+	 (prog-mode . envrc-mode))
   )
 
 (provide 'init-envrc)
